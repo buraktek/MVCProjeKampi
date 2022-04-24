5eb0252271715242a1ad3ad7a27054b7acbcdad7
burak.tekin <burak.tekin@lunatr.com>
2022-04-24 10:58:49 +0300

    Add 404 not found page and function

---
+++ MVCProjeKampi/Controllers/ErrorPagesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCProjeKampi.Controllers
+{
+    public class ErrorPagesController : Controller
+    {
+        public IActionResult Page403()
+        {
+            Response.StatusCode = 403;
+            //Response. = true;
+            return View();
+        }
+
+        public IActionResult Page404()
+        {
+            Response.StatusCode = 404;
+            //Response. = true;
+            return View();
+        }
+    }
+}
